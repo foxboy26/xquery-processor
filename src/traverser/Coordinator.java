51a0b272771c0becff347e0e0f9208d33de912f2
lzhiheng@eng.ucsd.edu <lzhiheng@eng.ucsd.edu@2af4dcd7-86be-a92e-4fc5-de52bf643ee4>
2013-06-01 03:38:57 +0000

    added printer

--- src/traverser/Coordinator.java
+++ src/traverser/Coordinator.java
@@ -2,9 +2,18 @@ package traverser;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 
+import org.apache.xerces.dom.DocumentImpl;
+import org.apache.xml.serialize.Method;
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.Document;
 
 import parser.ASTStart;
 import parser.XQueryParser;
@@ -17,13 +26,16 @@ public class Coordinator {
 		XQueryParser t;
 		try {
 			t = new XQueryParser(new FileInputStream(new File("xpath.txt")));
-			ArrayList<Node> data = new ArrayList<Node>();
 			ASTStart n = t.Start();
 			n.dump(">");
 			
+			System.out.println("Result:");
+			
+			ArrayList<Node> resultSet = new ArrayList<Node>();			
 			XQueryParserVisitor visitor = new XQueryVisitor();
-			data = (ArrayList<Node>) n.jjtAccept(visitor, data);
-			System.out.println(data);
+			resultSet = (ArrayList<Node>) n.jjtAccept(visitor, resultSet);
+			print(resultSet);
+			
 		} catch (Exception e) {
 			System.out.println("Oops.");
 			System.out.println(e.getMessage());
@@ -31,4 +43,23 @@ public class Coordinator {
 		}
 	}
 
+	public static void print(ArrayList<Node> result) {
+    PrintWriter writer= new PrintWriter(System.out);
+		XMLSerializer serializer = new XMLSerializer(writer, new OutputFormat(Method.XML, "UTF-8", true));
+    boolean first = true;
+		for (Node n : result) {
+			try {
+				if (first) {
+		      serializer.serialize((Element) n);
+		    	first = false;
+				} else {
+					System.out.println("------------------------");
+		      serializer.serialize((Element) n);
+				}
+      } catch (IOException e) {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+      }
+		}
+	}
 }
